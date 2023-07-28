e69466a56b4867f9b38adcfaedbf8119200aecbd
Olivier PERDRIX <olivierperdrix@live.fr>
2023-07-28 18:33:23 +0200

    Fetching dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,12 @@
 "use client"
 import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
+import useSWR from 'swr'
 
 const Dashboard = () => {
-  const [data, setData] = useState([])
-  const [err, setErr] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    const getData = async () => {
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
-        cache: 'no-store'
-      })
-      if (!res.ok) {
-        throw new Error('Failed to fetch data')
-      }
-      return res.json()
-    }
-  })
-
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+  const { data, error, isLoading } = useSWR(`https://jsonplaceholder.typicode.com/posts`, fetcher)
   console.log(data);
 
   return (
