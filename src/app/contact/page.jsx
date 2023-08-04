52beddcc3b6b7e7481debec3d31b38b0e4d7a14e
Olivier PERDRIX <116632018+operdrix@users.noreply.github.com>
2023-08-04 22:41:20 +0200

    ajout metadata

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,11 @@ import styles from './page.module.css'
 import Image from 'next/image'
 import Button from '@/components/button/Button'
 
+export const metadata = {
+  title: 'Olivier Perdrix Contact Informations',
+  description: 'Page de contact',
+}
+
 const Contact = () => {
   return (
     <div className={styles.container}>
